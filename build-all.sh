13fdfee1b27d3f6871badce2b6ab90a9c30d7994
Brian Sidebotham <brian.sidebotham@propellent.io>
2022-01-12 14:21:01 +0000

    Save result of build execution so we can correctly report on a failure

--- build-all.sh
+++ build-all.sh
@@ -10,9 +10,10 @@ for os in ${OS}; do
         echo -n "Building Python ${pyversion} for ${os}..."
         start=$(date +%s)
         ${scriptdir}/build.sh ${os} ${pyversion} > ${scriptdir}/build-${os}-${pyversion}.log 2>&1
+        result=$?
         finish=$(date +%s)
         minutes=$(( (${finish} - ${start}) / 60 ))
-        if [ $? -ne 0 ]; then
+        if [ ${result} -ne 0 ]; then
             echo "FAIL"
         else
             echo "OK (In ${minutes}min)"
