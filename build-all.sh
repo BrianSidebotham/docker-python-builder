ad19e862c3723359e779145b7ba48a944ba227d5
Brian Sidebotham <brian.sidebotham@gmail.com>
2022-02-23 22:51:16 +0000

    Update incorrect dependencies for all Ubuntu builds

--- build-all.sh
+++ build-all.sh
@@ -6,17 +6,14 @@ PYTHONVERSIONS="3.8.12 3.9.10 3.10.2"
 OS="ubuntu20.04 ubuntu21.10 centos8 centos7"
 
 for os in ${OS}; do
+    echo "Building Python for ${os}"
+    start=$(date +%s)
     for pyversion in ${PYTHONVERSIONS}; do
-        echo -n "Building Python ${pyversion} for ${os}..."
-        start=$(date +%s)
-        ${scriptdir}/build.sh ${os} ${pyversion} > ${scriptdir}/build-${os}-${pyversion}.log 2>&1
-        result=$?
-        finish=$(date +%s)
-        minutes=$(( (${finish} - ${start}) / 60 ))
-        if [ ${result} -ne 0 ]; then
-            echo "FAIL"
-        else
-            echo "OK (In ${minutes}min)"
-        fi
+        echo "Building Python ${pyversion}..."
+        ${scriptdir}/build.sh ${os} ${pyversion} > ${scriptdir}/build-${os}-${pyversion}.log 2>&1 &
     done
+    wait
+    finish=$(date +%s)
+    minutes=$(( (${finish} - ${start}) / 60 ))
+    echo "Complete in ${minutes} minutes"
 done
